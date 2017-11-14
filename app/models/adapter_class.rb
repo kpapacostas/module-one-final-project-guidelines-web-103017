a1d92998512bea01ac3e9bd8831202cd7f929ea2
github username <github email address>
2017-11-14 17:53:04 -0500

    functioning API link

--- app/models/adapter_class.rb
+++ app/models/adapter_class.rb
@@ -2,9 +2,10 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
-def get_recipes_from_api(cuisine)
-  recipes = RestClient.get('http://www.recipepuppy.com/api/?q="cuisine"')
+def get_recipes_from_api
+  recipes = RestClient.get('http://www.recipepuppy.com/api/?q=pasta&p=3')
   recipe_hash = JSON.parse(recipes)
+  recipe_hash
 end
-
-get_recipes_from_api("Mexican")
+binding.pry
+get_recipes_from_api
